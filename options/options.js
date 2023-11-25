b9eaa158466b54a2b81ab72535a481aaafd8f3ba
Crowdbotics <team@crowdbotics.com>
2023-11-25 05:48:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "secret-santa-44748",
+  url: "https://secret-santa-44748.botics.co",
+  api: "https://secret-santa-44748.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
